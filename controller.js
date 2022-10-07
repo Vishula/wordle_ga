af4dbbbc1f50302dd55c12acf4e2fb2ad3efff54
vishula1 <vgamaetige@gmail.com>
2022-10-07 12:08:51 +1000

    Getting tiles to color to according to the position, adding animation to gameboard

--- controller.js
+++ controller.js
@@ -1,3 +1,33 @@
+    // update the box with letters
+    function updateBoxWords(key) {
+        const currentBox = getCurrentArray()
+        if (currentBox && currentBox.length < 5) {
+            currentBox.push(key)
+            // gets the box with id 1 
+            const boxSpace = document.getElementById(String(spaceOfBox))
+            spaceOfBox = spaceOfBox + 1;
+            boxSpace.textContent = key
+        }
+
+    }
+
+    function delWord() {
+        const wordArray = getCurrentArray()
+
+
+        console.log(wordArray);
+        const currentBoxEl = document.getElementById(String(spaceOfBox - 1))
+        currentBoxEl.textContent = ''
+        spaceOfBox = spaceOfBox - 1
+        wordArray.pop()
+        console.log(currentBoxEl);
+        console.log(spaceOfBox);
+
+
+
+    }
+
+
 const keyboardKeys = document.querySelectorAll('.row button')
 function setupKeyboard() {
     for (let i = 0; i < keyboardKeys.length; i++) {
--- script.js
+++ script.js
@@ -2,52 +2,49 @@ createBoxes()
 setupKeyboard()
 
 // when hit enter key it should enter the word and move to next row if it has 5 letters in the word
-
 function enterWord() {
     // get the current array and write if statements to do 
     const wordArray = getCurrentArray()
     console.log(wordArray);
+    console.log(currentRow);
+    // join the array together
     const currentWord = wordArray.join('')
-    if (!wordle.includes(currentWord)) {
+    if (!wordle.includes(currentWord) && wordArray.length !== 5) {
         // console.log(secretWord, currentWord);
         alert("Word doesn't exist, delete and type again")
+
         return //early exit 
     }
-   
-    // current word and secret word 
-    for (let i = 0; i < 5; i++) {
-        let userGuess = currentWord.charAt(i)
-        let secretGuess = secretWord.charAt(i)
-        const idOfMatchingLetters = (getTitleID) + 1
 
-        if (userGuess === secretGuess) {
-            wordArray.map((char, index) => {
-                setTimeout(function colors() {
-                    // get id of specific box
-                    const idofLetter = idOfMatchingLetters + index
-                    // const elofLetter = document.getElementById(idofLetter).style.backgroundColor = 'green'
+    else {
+        guessWord.push([])
+        // flip animation 
+        const timeInterval = 150 
+        //  get the position of the first letter [id]  
+        const firstLetterOfId = getTitleID * 5 + 1 
 
-                })
+        // animation for gameboard. code from ian lenehan youtube account 
+        wordArray.map((letter, index) => {
+            setTimeout(() => {
+                const tileColor = tilteColors(letter, index)
+                const letterId = firstLetterOfId + index 
+                const getElementOfLetter = document.getElementById(letterId)
+                getElementOfLetter.classList.add()
+                getElementOfLetter.style = `background-color:${tileColor}; border-color:${tileColor}`
             })
-            // get current tile element ID and set its background to green 
-        } else if (currentWord.indexOf(guessWord) != -1) {
-            // current tiles to yellow 
-        } else {
-            // tiles to grey 
-        }
-
-
-
+        })
+        
     }
+
+
+
+    // if (wordArray.length !== 5) {
+    //     console.log(guessWord);
+    //     // add code to initalize the current array and remove all the letters from the square boxes
+    //     // currentWord.length = 0
+    //     alert("Enter a 5 letter word and try again")
 
     // }
-    if (wordArray.length !== 5) {
-        console.log(guessWord);
-        // add code to initalize the current array and remove all the letters from the square boxes
-        // currentWord.length = 0
-        alert("Enter a 5 letter word and try again")
-
-    }
 
 
     if (currentWord === secretWord) {
@@ -59,6 +56,7 @@ function enterWord() {
                 // const colorOfTiles = colorAdd(character, index)
                 // this gives ID of each specific box 
                 const idOfLetter = idOfMatchingLetters + index
+
                 // get element and apply grey to background
                 const elOfLetter = document.getElementById(idOfLetter).style.backgroundColor = 'green'
             })
@@ -68,67 +66,32 @@ function enterWord() {
 
 
     }
-    if (guessWord.length === 6) {
+    if (guessWord.length === 7) {
         alert(`You ran out of guesses. Word is ${secretWord}`)
     }
 
 
-    guessWord.push([])
-}
-// color function 
-
-
-// function colorAdd(guess, solution){
-//     let r = []
-//     for (let i = 0; i < guess.length; i++){
-//         let userGuessed = guess.charAt(i)
-//         let secretWordLetter = solution.charAt(i)
-
-//         console.log(userGuessed, secretWordLetter);
-//         if (userGuessed === secretWordLetter){
-//             r.push('Green')
-
-
-
-//         }else if (solution.indexOf(userGuessed)!=-1){
-//             r.push('Yellow')
-//         }
-//         else {
-//             r.push("Grey")
-//         }
-//     }
-//     return r 
-// }
-
-
-function delWord() {
-    const wordArray = getCurrentArray()
-    wordArray.pop()
-
-    console.log(wordArray);
-    const currentBoxEl = document.getElementById(String(spaceOfBox - 1))
-    currentBoxEl.innerHTML = ''
-    spaceOfBox = spaceOfBox - 1
-    console.log(currentBoxEl);
-    console.log(spaceOfBox);
-
-
-
+    
+    currentRow++
 }
 
+function tilteColors(letter, index) {
+    const correctLetterIndex = wordArray.includes(letter)
 
+    // letter doesn't exist in the secret word. Turn it to grey
+    if (!correctLetterIndex) {
+        return (58, 58, 60)
+    }
+    // 
 
-// update the box with letters
-function updateBoxWords(key) {
-    const currentBox = getCurrentArray()
-    if (currentBox && currentBox.length < 5) {
-        currentBox.push(key)
-        // gets the box with id 1 
-        const boxSpace = document.getElementById(String(spaceOfBox))
-        spaceOfBox = spaceOfBox + 1;
-        boxSpace.textContent = key
+    const letterIndex = secretWord.charAt(index)
+    console.log(letterIndex);
+    const correctPosition = letter == letterIndex
+
+    if (correctPosition) {
+        return 'rgb(83, 141, 78, 0.7)'
+    } else {
+        return 'rgb(181, 159, 59, 0.7)'
     }
 
 }
-
-
